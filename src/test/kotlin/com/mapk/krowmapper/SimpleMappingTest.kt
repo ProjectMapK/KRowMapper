2a41a6aa76010ad65a48a368a76db8e2e65890dd
wrongwrong <boranti1995@gmail.com>
2020-03-07 14:55:32 +0900

    単純なマッピングのテストを実装

---
+++ src/test/kotlin/com/mapk/krowmapper/SimpleMappingTest.kt
@@ -0,0 +1,34 @@
+package com.mapk.krowmapper
+
+import com.google.common.base.CaseFormat
+import com.mapk.core.KFunctionForCall
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import java.sql.ResultSet
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("単純なマッピングテスト")
+class SimpleMappingTest {
+    data class Dst(val fooId: Int, val strValue: String)
+
+    private fun camelToSnake(camel: String): String = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, camel)
+
+    @Test
+    @DisplayName("スネークケースsrc -> キャメルケースdst")
+    fun test() {
+        val resultSet = mockk<ResultSet>()
+        every { resultSet.getObject("foo_id", any<Class<*>>()) } returns 1
+        every { resultSet.getObject("str_value", any<Class<*>>()) } returns "str"
+
+        val result = KRowMapper(KFunctionForCall(::Dst), this::camelToSnake).mapRow(resultSet, 0)
+
+        assertEquals(1, result.fooId)
+        assertEquals("str", result.strValue)
+
+        verify(exactly = 1) { resultSet.getObject("foo_id", Int::class.java) }
+        verify(exactly = 1) { resultSet.getObject("str_value", String::class.java) }
+    }
+}
