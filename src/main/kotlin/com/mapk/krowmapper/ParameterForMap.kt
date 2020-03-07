f2ad0e0e764ea2fd3a54a19b7c2913cd08c9e227
wrongwrong <boranti1995@gmail.com>
2020-03-07 14:55:08 +0900

    ちゃんとクラスが取れていなかったため修正

--- src/main/kotlin/com/mapk/krowmapper/ParameterForMap.kt
+++ src/main/kotlin/com/mapk/krowmapper/ParameterForMap.kt
@@ -1,6 +1,7 @@
 package com.mapk.krowmapper
 
 import com.mapk.annotations.KPropertyAlias
+import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.findAnnotation
 
@@ -12,7 +13,7 @@ class ParameterForMap<T : Any> private constructor(
     companion object {
         fun newInstance(param: KParameter, propertyNameConverter: (String) -> String = { it }): ParameterForMap<*> {
             val name: String = (param.findAnnotation<KPropertyAlias>()?.value ?: propertyNameConverter(param.name!!))
-            return ParameterForMap(name, param.index, param.type.javaClass)
+            return ParameterForMap(name, param.index, (param.type.classifier as KClass<*>).java)
         }
     }
 }
