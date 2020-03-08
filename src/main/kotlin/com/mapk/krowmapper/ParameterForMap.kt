b94b30a57afe35cf1a27f134193d93ca47d6e003
wrongwrong <boranti1995@gmail.com>
2020-03-08 13:47:19 +0900

    変更を反映

--- src/main/kotlin/com/mapk/krowmapper/ParameterForMap.kt
+++ src/main/kotlin/com/mapk/krowmapper/ParameterForMap.kt
@@ -1,6 +1,6 @@
 package com.mapk.krowmapper
 
-import com.mapk.annotations.KPropertyAlias
+import com.mapk.annotations.KParameterAlias
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.findAnnotation
@@ -12,7 +12,7 @@ class ParameterForMap<T : Any> private constructor(
 ) {
     companion object {
         fun newInstance(param: KParameter, propertyNameConverter: (String) -> String = { it }): ParameterForMap<*> {
-            val name: String = (param.findAnnotation<KPropertyAlias>()?.value ?: propertyNameConverter(param.name!!))
+            val name: String = (param.findAnnotation<KParameterAlias>()?.value ?: propertyNameConverter(param.name!!))
             return ParameterForMap(name, param.index, (param.type.classifier as KClass<*>).java)
         }
     }
