8dd3abc9c869f268adf3d324bdc53c40df3117f2
wrongwrong <boranti1995@gmail.com>
2020-03-08 13:50:48 +0900

    呼び出しを簡略化

--- src/main/kotlin/com/mapk/krowmapper/KRowMapper.kt
+++ src/main/kotlin/com/mapk/krowmapper/KRowMapper.kt
@@ -3,13 +3,18 @@ package com.mapk.krowmapper
 import com.mapk.core.EnumMapper
 import com.mapk.core.KFunctionForCall
 import java.sql.ResultSet
+import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
 import org.springframework.jdbc.core.RowMapper
 
-class KRowMapper<T>(
+class KRowMapper<T : Any> private constructor(
     private val function: KFunctionForCall<T>,
     propertyNameConverter: (String) -> String = { it }
 ) : RowMapper<T> {
+    constructor(function: KFunction<T>, propertyNameConverter: (String) -> String = { it }) : this(
+        KFunctionForCall(function), propertyNameConverter
+    )
+
     private val parameters: List<ParameterForMap<*>> = function.parameters
         .filter { it.kind != KParameter.Kind.INSTANCE }
         .map { ParameterForMap.newInstance(it, propertyNameConverter) }
