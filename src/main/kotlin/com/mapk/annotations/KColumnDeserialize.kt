7c46ee030a687eb89674e10fbb3093204f1a0c2c
wrongwrong <boranti1995@gmail.com>
2020-03-14 21:35:35 +0900

    デシリアライザーの実装は難易度が高すぎるので一旦保留

--- src/main/kotlin/com/mapk/annotations/KColumnDeserialize.kt
+++
@@ -1,8 +0,0 @@
-package com.mapk.annotations
-
-import com.mapk.krowmapper.KColumnDeserializer
-import kotlin.reflect.KClass
-
-@Target(AnnotationTarget.VALUE_PARAMETER)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class KColumnDeserialize(val deserializer: KClass<out KColumnDeserializer<*, *>>)
--- src/main/kotlin/com/mapk/krowmapper/KColumnDeserializer.kt
+++
@@ -1,5 +0,0 @@
-package com.mapk.krowmapper
-
-abstract class KColumnDeserializer<Src : Any, Dst : Any> {
-    abstract fun deserialize(column: Src): Dst?
-}
