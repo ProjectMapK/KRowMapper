b3b72aca5be64d078e8111a478ac79c550641462
wrongwrong <boranti1995@gmail.com>
2020-03-20 10:11:56 +0900

    仮でデシリアライザーを取るように修正

--- src/main/kotlin/com/mapk/krowmapper/ParameterForMap.kt
+++ src/main/kotlin/com/mapk/krowmapper/ParameterForMap.kt
@@ -2,19 +2,22 @@ package com.mapk.krowmapper
 
 import com.mapk.core.getAliasOrName
 import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
 
 class ParameterForMap private constructor(
     val name: String,
     val param: KParameter,
-    val clazz: Class<*>
+    val clazz: Class<*>,
+    private val deserializer: KFunction<*>?
 ) {
     companion object {
         fun newInstance(param: KParameter, propertyNameConverter: (String) -> String = { it }): ParameterForMap {
             return ParameterForMap(
                 propertyNameConverter(param.getAliasOrName()!!),
                 param,
-                (param.type.classifier as KClass<*>).java
+                (param.type.classifier as KClass<*>).java,
+                null
             )
         }
     }
