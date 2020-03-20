77f11f7f1b4aca56df2c72df5b271ebb8d50bcc0
wrongwrong <boranti1995@gmail.com>
2020-03-20 10:46:58 +0900

    デシリアライザーの取得を追加

--- src/main/kotlin/com/mapk/krowmapper/ParameterForMap.kt
+++ src/main/kotlin/com/mapk/krowmapper/ParameterForMap.kt
@@ -1,11 +1,19 @@
 package com.mapk.krowmapper
 
+import com.mapk.annotations.KColumnDeserializer
 import com.mapk.core.EnumMapper
+import com.mapk.core.KFunctionWithInstance
 import com.mapk.core.getAliasOrName
+import java.lang.IllegalArgumentException
 import java.sql.ResultSet
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
+import kotlin.reflect.full.companionObjectInstance
+import kotlin.reflect.full.functions
+import kotlin.reflect.full.staticFunctions
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.jvmName
 
 class ParameterForMap private constructor(
     val param: KParameter,
@@ -16,7 +24,15 @@ class ParameterForMap private constructor(
     private val deserializer: KFunction<*>?
 
     init {
-        deserializer = null
+        val deserializers = deserializerFromConstructors(kClazz) +
+                deserializerFromStaticMethods(kClazz) +
+                deserializerFromCompanionObject(kClazz)
+
+        deserializer = when {
+            deserializers.isEmpty() -> null
+            deserializers.size == 1 -> deserializers.single()
+            else -> throw IllegalArgumentException("Find multiple deserializer from ${kClazz.jvmName}")
+        }
     }
 
     fun getObject(rs: ResultSet): Any? = when {
@@ -36,4 +52,36 @@ class ParameterForMap private constructor(
             )
         }
     }
+}
+
+private fun <T> Collection<KFunction<T>>.getDeserializerFromFunctions(): Collection<KFunction<T>> {
+    return filter { it.annotations.any { annotation -> annotation is KColumnDeserializer } }
+        .map { func ->
+            func.isAccessible = true
+            func
+        }
+}
+
+private fun <T : Any> deserializerFromConstructors(clazz: KClass<T>): Collection<KFunction<T>> {
+    return clazz.constructors.getDeserializerFromFunctions()
+}
+
+@Suppress("UNCHECKED_CAST")
+private fun <T : Any> deserializerFromStaticMethods(clazz: KClass<T>): Collection<KFunction<T>> {
+    val staticFunctions: Collection<KFunction<T>> = clazz.staticFunctions as Collection<KFunction<T>>
+    return staticFunctions.getDeserializerFromFunctions()
+}
+
+@Suppress("UNCHECKED_CAST")
+private fun <T : Any> deserializerFromCompanionObject(clazz: KClass<T>): Collection<KFunction<T>> {
+    return clazz.companionObjectInstance?.let { companionObject ->
+        companionObject::class.functions
+            .filter { it.annotations.any { annotation -> annotation is KColumnDeserializer } }
+            .map { function ->
+                KFunctionWithInstance(
+                    function,
+                    companionObject
+                ) as KFunction<T>
+            }.toSet()
+    } ?: emptySet()
 }
