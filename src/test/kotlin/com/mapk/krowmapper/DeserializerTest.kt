0aa28c500739177fea400ec6ecceaf4e7eb6dc5d
wrongwrong <boranti1995@gmail.com>
2020-03-22 18:55:26 +0900

    デシリアライザーのテストを追加

---
+++ src/test/kotlin/com/mapk/krowmapper/DeserializerTest.kt
@@ -0,0 +1,50 @@
+package com.mapk.krowmapper
+
+import com.mapk.deserialization.KColumnDeserializeBy
+import com.mapk.deserialization.KColumnDeserializer
+import io.mockk.every
+import io.mockk.mockk
+import java.sql.ResultSet
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName("アノテーションによるデシリアライザー指定のテスト")
+class DeserializerTest {
+    @Target(AnnotationTarget.VALUE_PARAMETER)
+    @Retention(AnnotationRetention.RUNTIME)
+    @MustBeDocumented
+    @KColumnDeserializeBy(LocalDateTimeDeserializerImpl::class)
+    annotation class LocalDateTimeDeserializer(val pattern: String = "yyyy-MM-dd'T'HH:mm:ss")
+
+    class LocalDateTimeDeserializerImpl(
+        annotation: LocalDateTimeDeserializer
+    ) : KColumnDeserializer<LocalDateTimeDeserializer, String, LocalDateTime>(annotation) {
+        private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern(annotation.pattern)
+
+        override val srcClass: Class<String> = String::class.javaObjectType
+
+        override fun deserialize(source: String?): LocalDateTime? {
+            return source?.let {
+                LocalDateTime.parse(it, formatter)
+            }
+        }
+    }
+
+    data class Dst(@LocalDateTimeDeserializer val dateTime: LocalDateTime)
+
+    @Test
+    fun test() {
+        val resultSet = mockk<ResultSet>()
+        every { resultSet.getObject("dateTime", any<Class<*>>()) } returns "2020-02-01T01:23:45"
+
+        val result = KRowMapper(::Dst).mapRow(resultSet, 0)
+
+        assertEquals(
+            LocalDateTime.parse("2020-02-01T01:23:45", DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")),
+            result.dateTime
+        )
+    }
+}
