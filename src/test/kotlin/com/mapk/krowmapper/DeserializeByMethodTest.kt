0ae78568b998f24b320c94003b39ef6e05c7e049
wrongwrong <boranti1995@gmail.com>
2020-03-28 16:07:33 +0900

    セカンダリコンストラクタを用いるテストを追加

--- src/test/kotlin/com/mapk/krowmapper/DeserializeByMethodTest.kt
+++ src/test/kotlin/com/mapk/krowmapper/DeserializeByMethodTest.kt
@@ -16,11 +16,16 @@ class DeserializeByMethodTest {
             fun factory(bar: String) = ByCompanionObject(bar.toInt())
         }
     }
+    data class BySecondaryConstructor(val quxShort: Short) {
+        @KColumnDeserializer
+        constructor(qux: String) : this(qux.toShort())
+    }
 
     data class Dst(
         val foo: ByConstructor,
         val bar: ByCompanionObject,
-        val baz: ByStaticMethod
+        val baz: ByStaticMethod,
+        val qux: BySecondaryConstructor
     )
 
     @Test
@@ -30,11 +35,13 @@ class DeserializeByMethodTest {
         every { resultSet.getObject("foo", any<Class<*>>()) } returns "foo"
         every { resultSet.getObject("bar", any<Class<*>>()) } returns "123"
         every { resultSet.getObject("baz", any<Class<*>>()) } returns 321
+        every { resultSet.getObject("qux", any<Class<*>>()) } returns "777"
 
         val result = KRowMapper(::Dst).mapRow(resultSet, 0)
 
         Assertions.assertEquals("foo", result.foo.fooString)
         Assertions.assertEquals(123, result.bar.barInt)
         Assertions.assertEquals("321", result.baz.bazString)
+        Assertions.assertEquals(777, result.qux.quxShort)
     }
 }
