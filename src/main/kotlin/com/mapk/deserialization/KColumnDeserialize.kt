49c59a77bf941faf303f14855b0ebfda17403b8c
wrongwrong <boranti1995@gmail.com>
2020-05-23 21:05:25 +0900

    不要な上界の削除

--- src/main/kotlin/com/mapk/deserialization/KColumnDeserialize.kt
+++ src/main/kotlin/com/mapk/deserialization/KColumnDeserialize.kt
@@ -7,7 +7,7 @@ import kotlin.reflect.KClass
 @MustBeDocumented
 annotation class KColumnDeserializeBy(val deserializer: KClass<out AbstractKColumnDeserializer<*, *, *>>)
 
-abstract class AbstractKColumnDeserializer<A : Annotation, S : Any, D : Any>(protected val annotation: A) {
+abstract class AbstractKColumnDeserializer<A : Annotation, S : Any, D>(protected val annotation: A) {
     abstract val srcClass: Class<S>
     abstract fun deserialize(source: S?): D?
 }
