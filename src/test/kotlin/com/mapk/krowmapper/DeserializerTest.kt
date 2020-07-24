614319a5fccabf69c646d382fc39d9e772bbb943
wrongwrong <boranti1995@gmail.com>
2020-07-24 18:21:32 +0900

    null入力時のテストケースを追加

--- src/test/kotlin/com/mapk/krowmapper/DeserializerTest.kt
+++ src/test/kotlin/com/mapk/krowmapper/DeserializerTest.kt
@@ -8,6 +8,7 @@ import java.sql.ResultSet
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
@@ -29,10 +30,11 @@ class DeserializerTest {
         override fun deserialize(source: String): LocalDateTime = LocalDateTime.parse(source, formatter)
     }
 
-    data class Dst(@LocalDateTimeDeserializer val dateTime: LocalDateTime)
+    data class Dst(@LocalDateTimeDeserializer val dateTime: LocalDateTime?)
 
     @Test
-    fun test() {
+    @DisplayName("正常に変換した場合")
+    fun isCollect() {
         val resultSet = mockk<ResultSet>()
         every { resultSet.getObject("dateTime", any<Class<*>>()) } returns "2020-02-01T01:23:45"
 
@@ -42,5 +44,17 @@ class DeserializerTest {
             LocalDateTime.parse("2020-02-01T01:23:45", DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")),
             result.dateTime
         )
+    }
+
+    @Test
+    @DisplayName("取得結果がnullだった場合")
+    fun isNull() {
+        val resultSet = mockk<ResultSet> {
+            every { getObject("dateTime", any<Class<*>>()) } returns null
+        }
+
+        val result = KRowMapper(::Dst).mapRow(resultSet, 0)
+
+        assertNull(result.dateTime)
     }
 }
