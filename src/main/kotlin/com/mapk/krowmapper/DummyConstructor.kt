87f1b97a6c3499d8505fde59153f55094a2eefee
wrongwrong <boranti1995@gmail.com>
2021-04-24 17:13:47 +0900

    ConversionService付きコンストラクタを追加

--- src/main/kotlin/com/mapk/krowmapper/DummyConstructor.kt
+++ src/main/kotlin/com/mapk/krowmapper/DummyConstructor.kt
@@ -1,6 +1,13 @@
 package com.mapk.krowmapper
 
+import org.springframework.core.convert.ConversionService
 import com.mapk.krowmapper.KRowMapper as Mapper
+
+@Suppress("FunctionName")
+inline fun <reified T : Any> KRowMapper(
+    conversionService: ConversionService,
+    noinline parameterNameConverter: ((String) -> String)? = null
+) = Mapper(T::class, conversionService, parameterNameConverter)
 
 @Suppress("FunctionName")
 inline fun <reified T : Any> KRowMapper(
--- src/main/kotlin/com/mapk/krowmapper/KRowMapper.kt
+++ src/main/kotlin/com/mapk/krowmapper/KRowMapper.kt
@@ -2,20 +2,40 @@ package com.mapk.krowmapper
 
 import com.mapk.core.KFunctionForCall
 import com.mapk.core.toKConstructor
+import org.springframework.core.convert.ConversionService
+import org.springframework.core.convert.support.DefaultConversionService
 import org.springframework.jdbc.core.RowMapper
 import java.sql.ResultSet
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 
-class KRowMapper<T : Any> private constructor(private val function: KFunctionForCall<T>) : RowMapper<T> {
-    constructor(function: KFunction<T>, parameterNameConverter: ((String) -> String)? = null) : this(
-        KFunctionForCall(function, parameterNameConverter)
-    )
+class KRowMapper<T : Any> private constructor(
+    private val function: KFunctionForCall<T>,
+    conversionService: ConversionService?
+) : RowMapper<T> {
+    constructor(
+        function: KFunction<T>,
+        conversionService: ConversionService,
+        parameterNameConverter: ((String) -> String)? = null
+    ) : this(KFunctionForCall(function, parameterNameConverter), conversionService)
 
-    constructor(clazz: KClass<T>, parameterNameConverter: ((String) -> String)? = null) : this(
-        clazz.toKConstructor(parameterNameConverter)
-    )
+    constructor(
+        function: KFunction<T>,
+        parameterNameConverter: ((String) -> String)? = null
+    ) : this(KFunctionForCall(function, parameterNameConverter), null)
 
+    constructor(
+        clazz: KClass<T>,
+        conversionService: ConversionService,
+        parameterNameConverter: ((String) -> String)? = null
+    ) : this(clazz.toKConstructor(parameterNameConverter), conversionService)
+
+    constructor(
+        clazz: KClass<T>,
+        parameterNameConverter: ((String) -> String)? = null
+    ) : this(clazz.toKConstructor(parameterNameConverter), null)
+
+    private val conversionService: ConversionService = conversionService ?: DefaultConversionService.getSharedInstance()
     private val parameters: List<ParameterForMap<*, *>> =
         function.requiredParameters.map { ParameterForMap.newInstance(it) }
 
